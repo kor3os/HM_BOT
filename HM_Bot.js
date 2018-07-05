01c5cbe34fd0c4cf7f117c27fdfdca62c5eca688
olivier <oliviersenn6@gmail.com>
2018-07-05 03:24:47 +0200

    changement de couleur pour les donateurs

--- HM_Bot.js
+++ HM_Bot.js
@@ -16,15 +16,19 @@
 
   //warned users
   const fs = require('fs');
-  var warnedUsers = new Map();
+  const util = require('util');
+  var warnedUsers;
+
 
   function saveWarnedUsers() {
-    fs.writeFile('warns.json', JSON.stringify(Array.from(warnedUsers.entries())), 'utf-8');
+    fs.writeFileSync('warns.json', JSON.stringify(Array.from(warnedUsers.entries())), 'utf-8');
     console.log(`saved ${warnedUsers.size} warn entries`);
   }
 
-  function restoreWarnedUsers() {
-    warnedUsers = new Map(JSON.parse(fs.readFile('warns.json', 'utf-8')));
+  function restoreWarnedUsers(){
+    var text = fs.readFileSync('warns.json');
+    warnedUsers = new Map(JSON.parse(text));
+    console.log(warnedUsers);
     console.log(`restored ${warnedUsers.size} warn entries`);
   }
 
@@ -32,43 +36,37 @@
 
 function warnMember(member){ //warn a member and mute him if necessary
   console.log(`warning user ${member.username}`);
-  if (!warnedUsers.has(member.toString())) {
-    warnedUsers.set(member.toString(), 1);
+  if (!warnedUsers.has(member.toString())) { // is he already warned?
+    warnedUsers.set(member.toString(), 1); // if not, add him to the list of warned
   }else{
     warnedUsers.set(member.toString(), warnedUsers.get(member.toString()) +1);
     if (warnedUsers.get(member.toString())>=3) {
       member.addRole('463379709049176094', "3rd warning").catch(console.error);// id of @Muted on HM.
       warnedUsers.delete(member.toString());
     }
-    saveWarnedUsers();
   }
+  saveWarnedUsers();
+}
+
+function cleanUpColorRoles(guild){
+  guild.roles.forEach((value) => {
+    if (value.name.includes("dncolor") && value.members.size == 0) {
+      value.delete()
+    }
+  });
+}
+
+
 
   //Runs on bot start
-  bot.on("ready", () => {
-    bot.once("ready", () => {
+  bot.once("ready", () => {
       console.log(`Bot started ! ${bot.users.size} users.`);
       bot.user.setActivity('Saucisse');
       restoreWarnedUsers();
-    })
-  });
-
-  bot.on("guildCreate", guild => {
-    console.log(`Joined guild ${guild.name} // id : ${guild.id}`);
   });
 
 
-  /* bot.on("message", (message) => {
-    if(message.author.bot) return;
 
-    if(message.channel.id = "388795219560103948") {
-       if(message.member.roles.exists("name", "Généraux"))
-        {
-          console.log(`Message kept due to admin role`)
-         return;
-       } else {
-        message.delete(1);
-       }
-    } */
 
   //HELLO
   bot.on("message", (message) => {
@@ -79,17 +77,69 @@ function warnMember(member){ //warn a member and mute him if necessary
       message.delete().catch(console.error);
     }
 
-    //Deleting "@everyone" made by random people
-//Broken
+    if (message.content.startsWith(config.prefixu)) { //user commands
+      var commandandargs = message.content.substring(2).split(" "); //split the command and args
+      var command = commandandargs[0]; //alias to go faster
+      if (command == "color") {
+        if (true/*user has donator role*/) { //FIXME
+          if (commandandargs.length == 2) { // i want exactly 1 argument
+            var role = message.member.roles.find(val=> val.name.includes("dncolor")); //find the user's color role if there is one
+            if (role) {
+              message.member.removeRole(role)
+              .then((member) => {
+                setTimeout(cleanUpColorRoles, 500, message.guild);
+              })
+              .catch(console.error);
+            }
+            if (commandandargs[1] != "reset") { // reset is not a color, allow people to just remove it
+              role = message.guild.roles.find(val => val.name === "dncolor"+commandandargs[1]);
+              if (role) {
+                message.member.addRole(role);
+              }else{}
+                message.guild.createRole({name: 'dncolor'+commandandargs[1],
+                                          color: commandandargs[1],
+                                          hoist: false,
+                                          position: message.member.roles.find('name', 'Donateur').position +1, // 1 au dessus du role donateur
+                                          mentionable: false})
+                .then((role) => {
+                  message.member.addRole(role);
+                });
+              }
+            }
+          }else{ // le mec a le droit mais il sait pas faire
+            message.reply("Example: `color #FF4200`");
+          }
+        }else { // le mec a pas le droit
+          message.reply("Vous devez etre donateur pour utiliser cette commande.");
+        }
+    }
+
+
+
+    if (message.content.startsWith(config.prefixm)) { // mod commands
+      // TODO: check for user's role
+      var commandandargs = message.content.substring(3).split(" "); //split the command and args
+      var command = commandandargs[0]; //alias to go faster
+      if (command === "warn") { //FIXME
+        warnMember(message.member);
+      }
+
+
+    }
+
+
+
+//     Deleting "@everyone" made by random people
+// Broken
     if (message.content.indexOf("@everyone") == 0) {
       if (message.member.roles.has("463382995466715136") || message.member.roles.has("463383000768446485")) {
         return;
       } else {
         var st = message.content;
-        message.delete().then(msg => console.log(`Deleted message from $(msg.author.username) with content = $(msg.content);; content check = ` + st));
+        message.delete().then(msg => console.log(`Deleted message from ${msg.author.username} with content = ${msg.content};; content check = ` + st));
       }
     }
-  })
+  });
 
 
   bot.login(token.token); //Yes.
