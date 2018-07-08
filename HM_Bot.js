df7547804430a85c82a8c84b940be37e7d68d805
Koreos <salisalut@laposte.net>
2018-07-08 20:57:29 +0200

    Deleting everyone
    Added autodelete of everyone made by retards

--- HM_Bot.js
+++ HM_Bot.js
@@ -10,22 +10,22 @@
   //Bot client.
   const bot = new Discord.Client();
 
-  //contains bot prefix
+  //Contains bot prefix
   const config = require("./config.json");
   const token = require("./token.json");
 
-  //warned users
+  //Warned users
   const fs = require('fs');
   const util = require('util');
   var warnedUsers;
 
 
-  function saveWarnedUsers() {
+  /*function saveWarnedUsers() {
     fs.writeFileSync('warns.json', JSON.stringify(Array.from(warnedUsers.entries())), 'utf-8');
     console.log(`saved ${warnedUsers.size} warn entries`);
   }
 
-  function restoreWarnedUsers(){
+  function restoreWarnedUsers() {
     var text = fs.readFileSync('warns.json');
     warnedUsers = new Map(JSON.parse(text));
     console.log(warnedUsers);
@@ -33,94 +33,93 @@
   }
 
 
-
-function warnMember(member){ //warn a member and mute him if necessary
-  console.log(`warning user ${member.username}`);
-  if (!warnedUsers.has(member.toString())) { // is he already warned?
-    warnedUsers.set(member.toString(), 1); // if not, add him to the list of warned
-  }else{
-    warnedUsers.set(member.toString(), warnedUsers.get(member.toString()) +1);
-    if (warnedUsers.get(member.toString())>=3) {
-      member.addRole('463379709049176094', "3rd warning").catch(console.error);// id of @Muted on HM.
-      warnedUsers.delete(member.toString());
+  function warnMember(member) { //warn a member and mute him if necessary
+    console.log(`warning user ${member.username}`);
+    if (!warnedUsers.has(member.toString())) { // is he already warned?
+      warnedUsers.set(member.toString(), 1); // if not, add him to the list of warned
+    } else {
+      warnedUsers.set(member.toString(), warnedUsers.get(member.toString()) + 1);
+      if (warnedUsers.get(member.toString()) >= 3) {
+        member.addRole('463379709049176094', "3rd warning").catch(console.error); // id of @Muted on HM.
+        warnedUsers.delete(member.toString());
+      }
     }
+    saveWarnedUsers();
   }
-  saveWarnedUsers();
-}
+  */
 
-function cleanUpColorRoles(guild){
-  guild.roles.forEach((value) => {
-    if (value.name.includes("dncolor") && value.members.size == 0) {
-      value.delete()
-    }
-  });
-}
-
+  function cleanUpColorRoles(guild) {
+    guild.roles.forEach((value) => {
+      if (value.name.includes("dncolor") && value.members.size == 0) {
+        value.delete()
+      }
+    });
+  }
 
 
   //Runs on bot start
   bot.once("ready", () => {
-      console.log(`Bot started ! ${bot.users.size} users.`);
-      bot.user.setActivity('Saucisse');
-      restoreWarnedUsers();
+    console.log(`Bot started ! ${bot.users.size} users.`);
+    bot.user.setActivity('Etre en beta fermée');
+    //restoreWarnedUsers();
   });
-
-
 
 
   //HELLO
   bot.on("message", (message) => {
 
     if (message.author.bot) return;
-    if (message.content.length >= 1000) { // degager les messages de 1000+ chars
+    if (message.content.length >= 1000) { //Degager les messages de 1000+ chars
       warnMember(message.member);
       message.delete().catch(console.error);
     }
 
-    if (message.content.startsWith(config.prefixu)) { //user commands
-      var commandandargs = message.content.substring(2).split(" "); //split the command and args
-      var command = commandandargs[0]; //alias to go faster
+    if (message.content.startsWith(config.prefixu)) { //User commands
+      var commandandargs = message.content.substring(2).split(" "); //Split the command and args
+      var command = commandandargs[0];  //Alias to go faster
       if (command == "color") {
-        if (message.member.roles.find('name', 'Donateur')) { // si on a le role donateur
-          if (commandandargs.length == 2) { // i want exactly 1 argument
-            var role = message.member.roles.find(val=> val.name.includes("dncolor")); //find the user's color role if there is one
+        if (message.member.roles.find('name', 'Donateur')) {  //Si on a le role donateur
+          if (commandandargs.length == 2) { //I want exactly 1 argument
+            var role = message.member.roles.find(val => val.name.includes("dncolor"));  //Find the user's color role if there is one
             if (role) {
               message.member.removeRole(role)
-              .then((member) => {
-                setTimeout(cleanUpColorRoles, 500, message.guild);// dans 1 demi seconde (attendre l'update), retirer tous les roles de couleur vides
-              })
-              .catch(console.error);
+                .then((member) => {
+                  setTimeout(cleanUpColorRoles, 500, message.guild);  //Dans 1 demi seconde (attendre l'update), retirer tous les roles de couleur vides
+                })
+                .catch(console.error);
             }
-            if (commandandargs[1] != "reset") { // reset is not a color, allow people to just remove it
-              role = message.guild.roles.find(val => val.name === "dncolor"+commandandargs[1]);
+            if (commandandargs[1] != "reset") { //Reset is not a color, allow people to just remove it
+              role = message.guild.roles.find(val => val.name === "dncolor" + commandandargs[1]);
               if (role) {
                 message.member.addRole(role);
-              }else{}
-                message.guild.createRole({name: 'dncolor'+commandandargs[1],
-                                          color: commandandargs[1],
-                                          hoist: false,
-                                          position: message.member.roles.find('name', 'Donateur').position +1, // 1 au dessus du role donateur
-                                          mentionable: false})
+              } else {}
+              message.guild.createRole({
+                  name: 'dncolor' + commandandargs[1],
+                  color: commandandargs[1],
+                  hoist: false,
+                  position: message.member.roles.find('name', 'Donateur').position + 1, //1 au dessus du role donateur
+                  mentionable: false
+                })
                 .then((role) => {
                   message.member.addRole(role);
                 });
-              }
             }
-          }else{ // le mec a le droit mais il sait pas faire
-            message.reply("Example: `color #FF4200`");
           }
-        }else { // le mec a pas le droit
-          message.reply("Vous devez etre donateur pour utiliser cette commande.");
+        } else {  //Le mec a le droit mais il sait pas faire
+          message.reply("Example: `color #FF4200`");
         }
-        //fin de la commande couleur
+      } else {  //Le mec a pas le droit
+        message.reply("Vous devez etre donateur pour utiliser cette commande.");
+      }
+      //Fin de la commande couleur
     }
 
 
 
-    if (message.content.startsWith(config.prefixm)) { // mod commands
+    if (message.content.startsWith(config.prefixm)) { //Mod commands
       // TODO: check for user's role
-      var commandandargs = message.content.substring(3).split(" "); //split the command and args
-      var command = commandandargs[0]; //alias to go faster
+      var commandandargs = message.content.substring(3).split(" "); //Split the command and args
+      var command = commandandargs[0];  //Alias to go faster
       if (command === "warn") { //FIXME
         warnMember(message.member);
       }
@@ -129,15 +128,15 @@ function cleanUpColorRoles(guild){
     }
 
 
-
-//     Deleting "@everyone" made by random people
-// Broken
-    if (message.content.indexOf("@everyone") == 0) {
-      if (message.member.roles.has("463382995466715136") || message.member.roles.has("463383000768446485")) {
+    //Deleting "@everyone" made by random people
+    // Broken
+    if (message.content.includes("@everyone")) {  //Si le message contient un everyone
+      if (message.member.roles.find('name', 'Salade de fruits') || message.member.roles.find('name', 'Généraux')) {
+        console.log("everyone ignoré => Salade ou Général");
         return;
       } else {
         var st = message.content;
-        message.delete().then(msg => console.log(`Deleted message from ${msg.author.username} with content = ${msg.content};; content check = ` + st));
+        message.delete().then(msg => console.log(`Deleted message from ${msg.author.username} with content = ${msg.content};; content check = ` + st)); //Echo the message in console
       }
     }
   });
