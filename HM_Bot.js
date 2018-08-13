5f4cdc9385d21afe32f1c6d734930ca36bf417ff
Olivier Senn <oliviersenn6@gmail.com>
2018-08-13 14:45:22 +0200

    fixed detection for protected names

--- HM_Bot.js
+++ HM_Bot.js
@@ -193,7 +193,7 @@
             message.reply("Erreur: " +e);
           }
         }else if(command == "setprotectedname"){
-          if(message.mentions.size === 1){
+          if(commandandargs[1].startsWith("<@")){
             protectednames.set(message.content.slice(21 + commandandargs[1].length), commandandargs[1].slice(3, -1));
             saveProtectedNames();
             message.reply(":ok_hand:");
